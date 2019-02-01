536264080d0e83c8d2cd66bc0270e4f9737f657f
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-02-01 11:38:21 -0500

    Initial setup of actions index.js for axios get request to fetch data.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,8 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
+
+export const FETCHING_SMURFS = "FETCHING_SMURFS";
+export const FETCHING_SMURFS_SUCCESSFUL = "FETCHING_SMURFS_SUCCESSFUL";
+export const FETCHING_SMURFS_FAILED = "FETCHING_SMURFS_SUCCESSFUL";
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +14,13 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: FETCHING_SMURFS });
+  axios
+    .get("http://localhost:3333/smurfs")
+    .then(res => console.log(res))
+    // .then(res => dispatch({ type: FETCHING_SMURFS_SUCCESSFUL, payload: res.data }))
+    .catch(err => console.log(err))
+    // .catch(err => dispatch({ type: FETCHING_SMURFS_FAILED, payload: err }));
+};
